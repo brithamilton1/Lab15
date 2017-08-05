f0de68b7903e779c9698d4cd320abc3f34114f51
Brittany <brithamilton1@gmail.com>
2017-08-05 15:54:01 -0400

    index controller

--- index.controller.js
+++ index.controller.js
@@ -1,1 +1,11 @@
 var app = angular.module("myApp");
+
+app.controller("IndexCtrl", function($scope, RedditFactory){
+  RedditFactory.getPosts()
+    .then(function(result){
+        Console.log(result)
+    })
+    .catch(function(error){
+      $scope.error = "there was an error getting posts"
+    });
+});
