a43f2c78ec7d700c9bc81d077f3e6e799777f098
Brittany <brithamilton1@gmail.com>
2017-08-05 15:28:22 -0400

    ngroute added

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-var app = angular.module("myApp", [])
+var app = angular.module("myApp", ["ngRoute"]);
+
+app.config(function($routeProvider, $locationProvider){
+  $routeProvider
+  .when("/index", {
+    templateUrl: "index.html",
+    controller: "IndexCtrl"
+  })
+  .when("/", {
+    templateUrl:"fizzbuzz/fizzbuzz.html",
+    controller: "FizzBuzzCtrl"
+  })
+  .otherwise({
+      redirectTo: "/index"
+    });
+
+    $locationProvider.hashPrefix("");
+  });
