e01d398f5d6c1b22da3c7a0115988696c530c95a
Brittany <brithamilton1@gmail.com>
2017-08-09 18:21:55 -0400

    updated reddit material

--- index.controller.js
+++ index.controller.js
@@ -1,14 +1,5 @@
 var app = angular.module("myApp");
-<<<<<<< HEAD
-=======
 
-app.controller("IndexCtrl", function($scope, RedditFactory){
-  RedditFactory.getPosts()
-    .then(function(result){
-        Console.log(result)
-    })
-    .catch(function(error){
-      $scope.error = "there was an error getting posts"
-    });
-});
->>>>>>> 58cdcca644f49f54625efa2e25d546f79d2b23a1
+app.controller("IndexCtrl", function(){
+
+})
--- index.js
+++ index.js
@@ -3,17 +3,17 @@ var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider, $locationProvider){
   $routeProvider
   .when("/", {
-    templateUrl: "index.html",
+    templateUrl: "main.html",
     controller: "IndexCtrl"
   })
-  .when("/fizzbuzz.html", {
+  .when("/fizzbuzz", {
     templateUrl:"/fizzbuzz.html",
     controller: "FizzBuzzControl"
   })
-  .when("/reddit.html", {
+  .when("/reddit", {
     templateUrl:"/reddit.html",
     controller: "RedditCtrl"
-
+  })
   .otherwise({
       redirectTo: "/index"
     });
